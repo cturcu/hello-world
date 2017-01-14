b2b010521043c852e8b38c4a23e73803c3a64e58
cturcu <cturcu.paii@gmail.com>
2017-01-14 14:26:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="pragma" content="no-cache" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | DiPaCult</title>
+	
+	<!-- core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	<link href="css/meniuri.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body class="homepage">
+    <header id="header">
+        <div class="top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-4">
+                        <div class="top-number"><p><i class="fa fa-phone-square"></i>  +40 744 79 10 75</p></div>
+                    </div>
+                    <div class="col-sm-6 col-xs-8">
+                       <div class="social">
+                            <ul class="social-share">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-skype"></i></a></li>
+
+                            </ul>
+ <!--                           <div class="search">
+                                <form role="form">
+                                    <input type="text" class="search-form" autocomplete="off" placeholder="Search">
+                                    <i class="fa fa-search"></i>
+                                </form>
+                           </div> -->
+						   <div class="btn-group">
+								<button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Langue <span class="caret"></span>
+								</button>
+								<ul class="dropdown-menu">
+									<li><a href="index.php">Roumain</a></li>
+									<li><a href="fr/index_fr.php">Fran&ccedil;ais</a></li>
+								</ul>
+							</div>
+
+                       </div>
+                    </div>
+					
+                </div>
+            </div><!--/.container-->
+        </div><!--/.top-bar-->
+
+<!--		
+        <nav class="navbar navbar-inverse" role="banner">
+            <div class="container">
+               <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!--/<a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
+					
+					 <a class="navbar-brand" href="index.html"><img src="images/Sigle.png" alt="logo"></a>
+
+                </div>
+				
+                <div class="collapse navbar-collapse navbar-right">
+					<ul class="nav navbar-nav">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li><a href="about-us.html">Actualitati</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">DiPaCult <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="blog-item.html">Despre proiect</a></li>
+                                <li><a href="pricing.html">Arii de analiza</a></li>
+                                <li><a href="404.html">Obiective strategice</a></li>
+                                <li><a href="shortcodes.html">Echipa</a></li>
+								<li><a href="instrumente.html">Instrumente</a></li>
+								<li><a href="shortcodes.html">Rezultate</a></li>
+                            </ul>
+                        </li>
+						
+						<li><a href="portfolio.html">Intalniri/Conferinte</a></li>
+                        
+                        <li><a href="blog.html">Publicatii</a></li>
+						<li><a href="blog.html">Galerie foto</a></li>
+                        <li><a href="contact-us.html">Contact</a></li>                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+-->	
+		<?php include("header.php"); ?>
+    </header><!--/header-->
+
+    <section id="main-slider" class="no-margin">
+        <div class="carousel slide">
+            <ol class="carousel-indicators">
+                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+                <li data-target="#main-slider" data-slide-to="1"></li>
+                <li data-target="#main-slider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+
+                <div class="item active" style="background-image: url(images/slider/bg1.jpg)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+								<!--
+                                    <h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+                                    <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
+                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+								-->
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!--<img src="images/slider/img1.png" class="img-responsive">-->
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->
+
+                <div class="item" style="background-image: url(images/slider/bg2.jpg)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+								<!--
+                                    <h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+                                    <h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
+                                    <a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                                -->
+								</div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!--<img src="images/slider/img2.png" class="img-responsive">-->
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->
+
+                <div class="item" style="background-image: url(images/slider/bg3.jpg)">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                <!--
+									<h1 class="animation animated-item-1">Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+									<h2 class="animation animated-item-2">Accusantium doloremque laudantium totam rem aperiam, eaque ipsa...</h2>
+									<a class="btn-slide animation animated-item-3" href="#">Read More</a>
+                                -->
+								</div>
+                            </div>
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <!--<img src="images/slider/img3.png" class="img-responsive">-->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/.item-->
+            </div><!--/.carousel-inner-->
+        </div><!--/.carousel-->
+        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
+            <i class="fa fa-chevron-left"></i>
+        </a>
+        <a class="next hidden-xs" href="#main-slider" data-slide="next">
+            <i class="fa fa-chevron-right"></i>
+        </a>
+    </section><!--/#main-slider-->
+
+
+    <section id="content">
+        <div class="container">
+            <div class="row">
+                	<section id="txt_left" class="col col-lg-8">
+						<p class="text_stg">Spa&#539;iul rural rom&#226;nesc este marcat, ca &#x219;i societatea &#238;n ansamblu, de o serie de transform&#259;ri profunde, at&#226;t la nivelul utiliz&#259;rii resurselor c&#226;t &#x219;i la nivelul modului &#238;n care locuitorii satelor se adapteaz&#259; cerin&#539;elor unor schimb&#259;ri radicale ale partenerilor de dezvoltare local&#259;. &#206;n cadrul acestui proces, strategiile de dezvoltare ignor&#259; particularit&#259;&#539;ile geografice &#x219;i culturale ale unor zone afectate de func&#539;ionalit&#259;&#539;i economice neadecvate sau de areale miniere abandonate.</p>	
+					</section>
+					
+					<section id="txt_right" class="col col-lg-4 hidden-sm">
+						<p>Proiectul identific&#259; prin ariile de analiz&#259; din Bucovina &#x219;i Belgia o abordare durabil&#259; a dezvolt&#259;rii spa&#539;iului rural, a particularit&#259;&#539;ilor geografice ale peisajelor culturale, &#238;n special ale celor cu fri&#x219;e miniere (arii miniere abandonate).</p>	
+					</section>
+            </div><!--/.row-->
+        </div><!--/.container-->
+    </section><!--/#content-->
+
+     <section id="bottom">
+        <div class="container wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Universitate</h3>
+                        <ul>
+                            <li><a target="_blank" href="http://www.usv.ro">Despre noi...</a></li>
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Suport</h3>
+                        <ul>
+                            <li><a href="contact.php">Intrebari</a></li>
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Echipa</h3>
+                        <ul>
+                            <li><a href="echipa.php">Echipa proiect</a></li>
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="widget">
+                        <h3>Parteneri</h3>
+                        <ul>
+                            <li><a target="_blank" href="http://www.ulg.ac.be/cms/c_5000/fr/accueil#">Universit&#233; de Li&#232;ge</a></li>
+                        </ul>
+                    </div>    
+                </div><!--/.col-md-3-->
+            </div>
+        </div>
+    </section><!--/#bottom-->
+
+    <footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+				<div class="col-sm-6">
+                    <p>&copy CTC Software</p>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="echipa.php">Despre noi</a></li>
+                        <li><a href="contact.php">Contactati-ne</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/wow.min.js"></script>
+</body>
+</html>
